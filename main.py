f3402959036a475c3180e6ff54eb04519c52c235
Anders Keis <anders.keis@gmail.com>
2023-10-28 19:28:40 +0200

    Second version with variables for settings

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
+# Remember to install the Pillow library first "pip install pillow"
 from PIL import Image
 from PIL import UnidentifiedImageError
 import os
 
-# input picture to be compressed, and the wanted quality, it will overwrite existing image
+#Set Quality 1-100
+chosen_quality = 70
+#Size of picture
+resize_divide_by = 2
+
+#Starting directory where your files are located
+path_to_pictures = "pic"
+
+# input picture to be compressed, and the wanted quality, it will overwrite existing image!
 def compress(jpg, new_quality):
     image = Image.open(jpg)
     width, height = image.size
-    new_size = (width//4, height//4)
+    new_size = (width//resize_divide_by, height//resize_divide_by)
     resized_image = image.resize(new_size)
     resized_image.save(jpg, optimize=True, quality=new_quality)
     print(jpg+" Has been optimized with the chosen quality")
 
 # Function to walk through nested folder, and give them as input to compress()
-def list_files(dir):
-#    r = []
+def compress_files(dir):
+
     for root, dirs, files in os.walk(dir):
         for name in files:
             try:
                 print(root+"/"+name)
                 
-                #compress(root+"/"+name, 70)
+                compress(root+"/"+name, chosen_quality)
             except UnidentifiedImageError:
                 print(root+"/"+name+" Is not working with Pillow")
                 continue
-#    return r
 
-# compress("pic/Border82-2.jpg", 70)
-list_files("pic")
+
+# 
+compress_files("pic")
